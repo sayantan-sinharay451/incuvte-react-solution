ef5ad8707037c536cd512d9ee1f6894fa09c5ab7
Sayantan Sinharay <sayantansinharay@gmail.com>
2025-11-03 18:32:45 +0530

    feat: Refactor tokenize function to streamline token pushing and enhance readability, passed all initial test cases.

--- src/helper/tokenizer.ts
+++ src/helper/tokenizer.ts
@@ -2,6 +2,18 @@ export function tokenize(expression: string): string[] {
   const tokens: string[] = [];
   let currToken = '';
 
+  // Method to push for number type tokens
+  function pushCurrentToken() {
+    if (currToken !== '') {
+      // Throws error for the multiple decimal points
+      if (isNaN(parseFloat(currToken)) || (currToken.match(/\./g) || []).length > 1) {
+        throw new Error(`Invalid number: ${currToken}`);
+      }
+      tokens.push(currToken);
+      currToken = '';
+    }
+  }
+
 
   for (let i = 0; i < expression.length; i++) {
     const char = expression[i];
@@ -10,18 +22,23 @@ export function tokenize(expression: string): string[] {
     }
     // Arithmetic symbols
     if (/[\+\*\/\^\%\(\)]/.test(char)) {
-      if (currToken) {
-        tokens.push(currToken);
-        currToken = '';
-      }
+      pushCurrentToken() // push the number tokens in the stack
       tokens.push(char);
+      // case where a number can be negative or '-' as operation
+    } else if (char === '-') {
+      pushCurrentToken();
+      // if its the start of exp., ( or have any opp. it will consider it as -ve number. 
+      if (tokens.length === 0 || /[\+\-\*\/\^\%\(]/.test(tokens[tokens.length - 1])) {
+        currToken += char;
+      } else {
+        tokens.push(char);
+      }
     } else {
       currToken += char;
     }
   }
-  if (currToken) {
-    tokens.push(currToken);
-  }
+
+  pushCurrentToken() // push any remaining numbers
 
   return tokens;
 }
