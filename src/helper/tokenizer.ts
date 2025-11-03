6744c428f866616284bee977653e3b124311d7b4
Sayantan Sinharay <sayantansinharay@gmail.com>
2025-11-03 18:20:19 +0530

    feat: Refactor tokenize function to improve token extraction and add comprehensive tests

--- src/helper/tokenizer.ts
+++ src/helper/tokenizer.ts
@@ -1,9 +1,27 @@
 export function tokenize(expression: string): string[] {
   const tokens: string[] = [];
+  let currToken = '';
+
 
   for (let i = 0; i < expression.length; i++) {
     const char = expression[i];
-    tokens.push(char);
+    if (char === ' ') {
+      continue;
+    }
+    // Arithmetic symbols
+    if (/[\+\*\/\^\%\(\)]/.test(char)) {
+      if (currToken) {
+        tokens.push(currToken);
+        currToken = '';
+      }
+      tokens.push(char);
+    } else {
+      currToken += char;
+    }
   }
+  if (currToken) {
+    tokens.push(currToken);
+  }
+
   return tokens;
 }
--- src/test/tokenizer.test.ts
+++ src/test/tokenizer.test.ts
@@ -5,4 +5,28 @@ describe("tokenize", () => {
   it("should tokenize a simple expression", () => {
     expect(tokenize("1 + 2")).toEqual(["1", "+", "2"]);
   });
+
+  it("should tokenize an expression with no spaces", () => {
+    expect(tokenize("1+2")).toEqual(["1", "+", "2"]);
+  });
+
+  it("should tokenize numbers with multiple digits", () => {
+    expect(tokenize("123 + 456")).toEqual(["123", "+", "456"]);
+  });
+
+  it("should tokenize a decimal number", () => {
+    expect(tokenize("1.23 + 4.56")).toEqual(["1.23", "+", "4.56"]);
+  });
+
+  it("should tokenize a negative number", () => {
+    expect(tokenize("-12 + 22")).toEqual(["-12", "+", "22"]);
+  })
+
+  it("should tokenize an expression with negative numbers", () => {
+    expect(tokenize("-12 + -22")).toEqual(["-12", "+", "-22"]);
+  });
+  it("should tokenize all operators", () => {
+    expect(tokenize("1+2-3*4/5^6%7")).toEqual(["1", "+", "2", "-", "3", "*", "4", "/", "5", "^", "6", "%", "7"]);
+  });
+
 });
