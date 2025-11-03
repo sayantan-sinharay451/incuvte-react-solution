53a6e61a1084c57809c60198539eee6c78caf886
Sayantan Sinharay <sayantansinharay@gmail.com>
2025-11-03 18:48:21 +0530

    feat: Add tests for tokenizing parentheses and error handling in tokenizer function

--- src/test/tokenizer.test.ts
+++ src/test/tokenizer.test.ts
@@ -25,8 +25,26 @@ describe("tokenize", () => {
   it("should tokenize an expression with negative numbers", () => {
     expect(tokenize("-12 + -22")).toEqual(["-12", "+", "-22"]);
   });
+
   it("should tokenize all operators", () => {
     expect(tokenize("1+2-3*4/5^6%7")).toEqual(["1", "+", "2", "-", "3", "*", "4", "/", "5", "^", "6", "%", "7"]);
   });
 
+  it("should tokenize parentheses", () => {
+    expect(tokenize("(1+2)*3")).toEqual(["(", "1", "+", "2", ")", "*", "3"]);
+  });
+
+  it("should throw an error for text inputs", () => {
+    expect(() => tokenize("invalid input")).toThrow("Invalid number: invalidinput");
+  });
+
+  it('should throw an error for a number with multiple decimal points', () => {
+    const input = '1.2.3';
+    expect(() => tokenize(input)).toThrow('Invalid number: 1.2.3');
+  });
+
+  it("should throw an error for invalid expression", () => {
+    const input = "1 (2 + 3)";
+    expect(() => tokenize(input)).toThrow("Invalid Expression: 1 (2 + 3)");
+  })
 });
